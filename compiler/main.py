3655f922173121f88ec656bfd8ed92069b982fd0
shmint <148349626+swissmint@users.noreply.github.com>
2024-03-17 14:52:21 +0100

    Enter

--- compiler/main.py
+++ compiler/main.py
@@ -4,5 +4,7 @@ while True:
 		text = input("interperter >: ")
 		result, error = basic.run("<stdin>", text)
     
-		if error: print(error.as_string())
-		else: print(result)
+		if error: 
+			print(error.as_string())
+		else: 
+			print(result)
